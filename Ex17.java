33caa855206f807b2445c4cbb48aa970576e0469
isabelamariapiva <isabela.piva1a@gmail.com>
2025-03-31 15:57:38 -0300

    a soma de 10 numeros

---
+++ Ex17.java
@@ -0,0 +1,22 @@
+/*
+Ex17
+ leia 10 números do usuário e calcule a soma desses números
+*/
+import java.util.Scanner;
+
+public class Ex17 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int soma = 0;
+        
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Digite o " + i + "º número: ");
+            int numero = scanner.nextInt();
+            soma += numero;
+        }
+        
+        System.out.println("A soma dos 10 números é: " + soma);
+        
+        scanner.close();
+    }
+}
