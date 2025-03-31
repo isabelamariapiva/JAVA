623a3679672e953f45cdab6762351ef1397193db
isabelamariapiva <isabela.piva1a@gmail.com>
2025-03-31 14:16:28 -0300

    dizer uma mensagem 10 vezes

---
+++ Ex14 (1).java
@@ -0,0 +1,14 @@
+/*
+Ex14
+dizer uma mensagem 10 vezes  
+
+*/
+
+public class Ex14{
+	public static void main(String[] args) {
+	
+     for (int i = 0; i < 10; i++) {
+        System.out.println("eu gosto de algaritimos ");
+	}
+ }
+}
