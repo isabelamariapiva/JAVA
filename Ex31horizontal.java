b207c3d817c4427ce7521b3fd77402196f95aef6
isabelamariapiva <isabela.piva1a@gmail.com>
2025-04-07 14:18:18 -0300

    1 ao 20 horizontal

---
+++ Ex31horizontal.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+ex.31
+Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. 
+Depois modifique o programa para que ele mostre os números um ao lado do outro.
+horizontal
+
+*******************************************************************************/
+public class Ex31horizontal
+ {
+ 	public static void main(String[] args) {
+ 	    int numero = 0;
+ 	while(numero <= 19){
+ 	    numero++;
+ 	    System.out.print (numero+ "º");
+ 	    
+ 	}
+ 	
+ 	}
+ }
