c8a1cf3130375d183d3ed0c23c7aaad87584e643
isabelamariapiva <isabela.piva1a@gmail.com>
2025-04-07 11:20:27 -0300

    Faça um programa, utilizando while, que mostre na tela de 0 até N, em que N é o limite inserido pelo usuário.

---
+++ Ex26.java
@@ -0,0 +1,22 @@
+/*
+Ex26
+Faça um programa, utilizando while, que mostre na tela de 0 até N, em que N é o limite inserido pelo usuário.
+*/
+import java.util.Scanner;
+
+public class Ex26 {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int numero=0;
+
+    System.out.print("Insira o numeo limite: " );
+    int n = scanner.nextInt();
+
+    while (numero <= n) {
+    System.out.println(numero);
+    numero++;  
+    }
+    
+ 
+    }
+}
