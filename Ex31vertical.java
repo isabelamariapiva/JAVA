79af5dee8ba9d5c00b7386e0e0059b3a8ddab4ea
isabelamariapiva <isabela.piva1a@gmail.com>
2025-04-07 14:12:47 -0300

    numero de 1 a 20 vertical

---
+++ Ex31vertical.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+Ex.31
+Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. 
+Depois modifique o programa para que ele mostre os números um ao lado do outro.
+vertical
+
+*******************************************************************************/
+public class Ex31vertical
+ {
+ 	public static void main(String[] args) {
+ 	    int numero = 0;
+ 	while(numero <= 19){
+ 	    numero++;
+ 	    System.out.println("numeros de 1 a 20: " +numero);
+ 	    
+ 	}
+ 	
+ 	}
+ }
+ 
