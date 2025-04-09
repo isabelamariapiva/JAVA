9b73cc6360d855331165787f99eff62adc96337f
isabelamariapiva <isabela.piva1a@gmail.com>
2025-04-09 08:49:02 -0300

    Exemplo while

---
+++ Ex34.java
@@ -0,0 +1,17 @@
+/*
+Ex34- Exemplo do while
+*/
+import java.util.Scanner;
+public class Ex34
+{
+public static void main(String[] args) {
+int contador = 0;
+
+do {
+    System.out.println(contador);
+    contador++;
+} while (contador <= 5);
+
+   
+  }
+}
