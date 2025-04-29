c69dca08a4a9a43cd9ea84d2dd1f20cb990a4d78
isabelamariapiva <isabela.piva1a@gmail.com>
2025-04-28 21:37:33 -0300

    Soma e Media 5 pessoas

---
+++ Somamedia5.java
@@ -0,0 +1,24 @@
+/*
+ 8-Leia a idade de cinco pessoas e exiba a soma e a média das idades. 
+*/
+import java.util.Scanner;
+public class Somamedia5
+{
+	public static void main(String[] args) {
+	    Scanner scanner = new Scanner (System.in);
+	    int soma = 0;
+	    double media;
+	    
+	    for (int i=0; i <5; i++) {
+		System.out.println("Digite a idade da " +(i + 1)+ "° pessoa");
+		int idade = scanner.nextInt();
+		soma += idade;
+	    }
+		 media = soma/5.0;
+	    
+		
+		System.out.println("A soma das idades é : "+soma);
+		System.out.println("A media das idades é : "+media);
+
+	}
+}
