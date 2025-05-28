085c51cff7e4c2a1a654edf0d228ac24680564ef
isabelamariapiva <isabela.piva1a@gmail.com>
2025-05-28 13:35:57 -0300

    Add files via upload
     leia uma matriz 5x5. quantos números pares existem na matriz.

--- Matriz1.java
+++ Matriz1.java
@@ -1,42 +1,57 @@
 /*
-matriz1- Desenvolver um programa que leia uma matriz 4 X 4
-   a) Verificar o número de linhas e colunas
-   b) Mostrar a matriz  formatada
+1. Crie um programa que leia uma matriz 5x5. Em seguida, conte quantos números pares
+existem na matriz.
 */
+
 public class Matriz1
 {
     public static void main(String[] args) {
-    ///defina a matriz 4X4
-    int [][] matriz = new int [4][4];
-       matriz[0][0]= 5;
-       matriz[0][1]= 6;
-       matriz[0][2]= 8;
-       matriz[0][3]= 4;
-       
-       matriz[1][0]= 8;
-       matriz[1][1]= 3;
-       matriz[1][2]= 1;
-       matriz[1][3]= 9;
-       
-       matriz[2][0]= 3;
-       matriz[2][1]= 2;
-       matriz[2][2]= 6;
-       matriz[2][3]= 6;
-       
-       matriz[3][0]= 9;
-       matriz[3][1]= 4;
-       matriz[3][2]= 7;
-       matriz[3][3]= 5;
-       
+    ///defina a matriz 5X5
+    int [][] matriz = new int [5][5];
+ 
+    matriz[0][0]= 5;
+    matriz[0][1]= 6;
+    matriz[0][2]= 8;
+    matriz[0][3]= 4;
+    matriz[0][4]= 7;
+ 
+    matriz[1][0]= 8;
+    matriz[1][1]= 3;
+    matriz[1][2]= 1;
+    matriz[1][3]= 9;
+    matriz[1][4]= 4;
+ 
+    matriz[2][0]= 3;
+    matriz[2][1]= 2;
+    matriz[2][2]= 6;
+    matriz[2][3]= 6;
+    matriz[2][4]= 5;
+   
+    matriz[3][0]= 9;
+    matriz[3][1]= 4;
+    matriz[3][2]= 7;
+    matriz[3][3]= 5;
+    matriz[3][4]= 2;
     
-    //imprimindo a matriz
-    System.out.print("\nMatriz 4X4: \n");
+    matriz[4][0]= 9;
+    matriz[4][1]= 4;
+    matriz[4][2]= 7;
+    matriz[4][3]= 5;
+    matriz[4][4]= 1;
+    
+    int par = 0;
+    
+    System.out.print("\nMatriz 5X5: \n");
     for(int i=0; i < matriz.length; i++){
-        for(int j =0; j < matriz[i].length; j++){
-            System.out.print(matriz[i][j]+"");
-        }
-        System.out.println();
+    for(int j =0; j < matriz[i].length; j++){
+    System.out.print(matriz[i][j]+"");
+    if(matriz[i][j] % 2 != 0){
+    par++;
     }
     }
+    System.out.println();
     }
-
+    System.out.println("\nOs numeros pares são:"+ par);
+    
+    }
+    }
